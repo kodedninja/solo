67927fa9b0183fa8b6a51a55ce28f301a82b5190
Hunor Karamán <hunix94@gmail.com>
2018-03-25 16:05:33 +0300

    description

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -5,12 +5,12 @@ module.exports = buttons
 function buttons (state, emit, title, textarea) {
 	return html`
 		<div>
-			<div class="1/2 dib" style="padding-right: 4%;">
+			<div class="1/2 dib" style="padding-right: 2%;">
 				<a href="#" class="1 my1 db tac f3" style="padding-top: 0.2rem;" onclick="${preview}">
 					${state.preview ? 'edit' : 'preview'}
 				</a>
 			</div>
-			<div class="1/2 dib" style="padding-left: 4%;">
+			<div class="1/2 dib" style="padding-left: 2%;">
 				<a href="#" class="1 my1 db tac f3 ${state.saved ? 'tclred' : 'tcred'}" style="padding-top: 0.2rem;" onclick="${save}">save</a>
 			</div>
 		</div>
